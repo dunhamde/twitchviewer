48e2f3ff30e788160c96e82ba8a2eb5eb8c7f76f
Derek Dunham <dunham.derek@gmail.com>
2018-08-31 11:36:40 -0700

    added new button to header for triggering featured streams transition

--- src/components/featured_button.js
+++ src/components/featured_button.js
@@ -2,9 +2,12 @@ import React, { Component } from "react";
 import "./style/featured_btn.css";
 
 class FeaturedButton extends Component {
+	onClick() {
+		alert("helllo :)");
+	}
 	render() {
 		return (
-			<a id="featured_btn" href="#btn">
+			<a onClick={this.onClick()} id="featured_btn" href="#btn">
 				Featured
 			</a>
 		);
