133b3cb6f377a7fc59b5e96f47ac5f8c2d54e197
Derek Dunham <dunham.derek@gmail.com>
2018-09-07 14:35:28 -0700

    Debounced search function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import _ from "lodash";
 import "./style/search.css";
 import { search } from "../actions/search";
 import { connect } from "react-redux";
@@ -6,95 +7,90 @@ import SearchResults from "./search_results";
 import { CSSTransition } from "react-transition-group";
 import onClickOutside from "react-onclickoutside";
 import {
-  getSearchStreams,
-  getSearchLoading
+	getSearchStreams,
+	getSearchLoading
 } from "../reducers/reducer_streams";
 
 class Search extends Component {
-  constructor(props) {
-    super(props);
-    this.onInputChange = this.onInputChange.bind(this);
-    this.timeout = 0;
-    this.state = {
-      term: "",
-      showResults: false
-    };
-  }
+	constructor(props) {
+		super(props);
+		//this.timeout = 0;
+		this.state = {
+			term: "",
+			showResults: false
+		};
+		this.debounceSearch = _.debounce(this.props.search, 300);
+	}
 
-  onInputChange(event) {
-    if (this.timeout) {
-      clearTimeout(this.timeout);
-    }
-    this.setState({
-      term: event.target.value,
-      showResults: true
-    });
-    if (this.state.term !== "") {
-      this.timeout = setTimeout(() => {
-        this.props.search(this.state.term);
-      }, 300);
-    }
-  }
+	onInputChange = event => {
+		this.setState({
+			term: event.target.value,
+			showResults: true
+		});
+		if (this.state.term !== "") {
+			this.debounceSearch(this.state.term);
+		}
+	};
 
-  handleSubmit(e) {
-    e.preventDefault();
-  }
+	handleSubmit(e) {
+		e.preventDefault();
+	}
 
-  handleClickOutside = () => {
-    this.setState({
-      showResults: false
-    });
-  };
+	handleClickOutside = () => {
+		this.setState({
+			showResults: false
+		});
+	};
 
-  handleSearchClick = () => {
-    this.setState({
-      showResults: true
-    });
-  };
+	handleSearchClick = () => {
+		this.setState({
+			showResults: true
+		});
+	};
 
-  render() {
-    return (
-      <div className="search-container">
-        <form onSubmit={this.handleSubmit} className="search-form">
-          <input
-            placeholder="Search for streams"
-            className="search-input"
-            size="40"
-            value={this.state.term}
-            onClick={this.handleSearchClick}
-            onChange={this.onInputChange}
-          />
-        </form>
+	render() {
+		return (
+			<div className="search-container">
+				<form onSubmit={this.handleSubmit} className="search-form">
+					<input
+						placeholder="Search for streams"
+						className="search-input"
+						size="40"
+						value={this.state.term}
+						onClick={this.handleSearchClick}
+						onChange={this.onInputChange}
+					/>
+				</form>
 
-        <CSSTransition
-          in={
-            this.props.loading === false &&
-            this.state.term &&
-            this.state.showResults
-          }
-          timeout={{
-            enter: 0,
-            exit: 500
-          }}
-          mountOnEnter
-          unmountOnExit
-          classNames="search"
-        >
-          <SearchResults />
-        </CSSTransition>
-      </div>
-    );
-  }
+				<CSSTransition
+					in={
+						this.props.loading === false &&
+						this.state.term &&
+						this.state.showResults
+					}
+					timeout={{
+						enter: 0,
+						exit: 500
+					}}
+					mountOnEnter
+					unmountOnExit
+					classNames="search"
+				>
+					<SearchResults />
+				</CSSTransition>
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    loading: getSearchLoading(state),
-    streams: getSearchStreams(state)
-  };
+	return {
+		loading: getSearchLoading(state),
+		streams: getSearchStreams(state)
+	};
 }
 
 export default connect(
-  mapStateToProps,
-  { search }
+	mapStateToProps,
+	{ search }
 )(onClickOutside(Search));
